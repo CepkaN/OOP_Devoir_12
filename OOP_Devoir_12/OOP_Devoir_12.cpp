a4ba53228b26c990c33149161c0c2f209b45b61a
user <user@ACER>
2023-08-26 22:29:53 +0500

    Добавьте файлы проекта.

---
+++ OOP_Devoir_12/OOP_Devoir_12.cpp
@@ -0,0 +1,21 @@
+﻿#include"Frazione.h"
+#include<memory>
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	try
+	{
+		std::shared_ptr<Frazione>fra = std::make_shared<Frazione>(12, 0);
+		std::shared_ptr<Frazione>fra2 = std::make_shared<Frazione>(34, 0);
+		std::shared_ptr<Frazione>fra3 = std::make_shared<Frazione>(7, 0);
+	}
+	catch (std::string& a) { std::cout << a; };
+	std::cout << "\n\n";
+
+
+
+
+
+	return 0;
+}
